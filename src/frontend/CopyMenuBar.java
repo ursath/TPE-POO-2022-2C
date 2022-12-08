42341881565975669932fe54127c78bf41d95136
agussang <msanguinetti@itba.edu.ar>
2022-12-08 19:01:21 -0300

    paso los botones de Toggle a Buttons, agrego sus iconos

--- src/frontend/CopyMenuBar.java
+++ src/frontend/CopyMenuBar.java
@@ -1,30 +1,38 @@
 package frontend;
 
+import com.sun.javafx.scene.web.skin.HTMLEditorSkin;
 import javafx.scene.Cursor;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.geometry.Insets;
+import java.util.ResourceBundle;
 
 public class CopyMenuBar extends HBox {
 
     // Botones Barra Superior
-    ToggleButton copyButton = new ToggleButton("Copiar");
-    ToggleButton pasteButton = new ToggleButton("Pegar");
-    ToggleButton cutButton = new ToggleButton("Cortar");
+    Button copyButton = new Button ("Copiar", getImage("copyIcon"));
+    Button cutButton = new Button ("Cortar", getImage("cutIcon"));
+    Button pasteButton = new Button ("Pegar", getImage("pasteIcon"));
 
     public CopyMenuBar(){
-        ToggleButton[] copyToolsArr = { cutButton,copyButton,pasteButton};
-        ToggleGroup tools = new ToggleGroup();
-        for(ToggleButton tool : copyToolsArr) {
-            tool.setMinWidth(90);
-            tool.setToggleGroup(tools);
+        Button[] copyToolsArr = { cutButton,copyButton,pasteButton};
+        for(Button tool : copyToolsArr) {
+            tool.setMinWidth(50);
             tool.setCursor(Cursor.HAND);
         }
 
         getChildren().addAll(copyToolsArr);
         setPadding(new Insets(5));
+        setSpacing(10);
         setStyle("-fx-background-color: #999");
     }
 
+    //Obtengo el icono correspondiente
+    private ImageView getImage(String imagePath){
+        String cutIconPath = ResourceBundle.getBundle(HTMLEditorSkin.class.getName()).getString(imagePath);
+        Image cutIcon = new Image(HTMLEditorSkin.class.getResource(cutIconPath).toString());
+        return new ImageView(cutIcon);
+    }
 }
