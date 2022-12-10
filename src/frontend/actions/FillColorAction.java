2ae47745e2156f753011aa38b1b52c84a0bd9912
solrodriguez <solrodriguez@itba.edu.ar>
2022-12-10 20:05:36 -0300

    FillColorAction

--- src/frontend/actions/FillColorAction.java
+++ src/frontend/actions/FillColorAction.java
@@ -5,8 +5,30 @@ import backend.model.Figure;
 import javafx.scene.paint.Color;
 
 public class FillColorAction {
+    Figure selectedFigure;
+    Color fillColor;
+    CanvasState canvasState;
 
-    FillColorAction(Figure selectedFigure, Color fillColor, CanvasState canvasState){
-
+    public FillColorAction(Figure selectedFigure, Color fillColor, CanvasState canvasState){
+        this.canvasState = canvasState;
+        this.fillColor = fillColor;
+        this.selectedFigure = selectedFigure;
     }
+
+    public void press() {
+        for(Figure figure : canvasState.figures()) {
+            if (figure == selectedFigure) {
+                figure.setFillColor(fillColor);
+            }
+        }
+    }
+
+    public void undo(){
+        // FALTA
+    }
+
+    public String toString(){
+        return String.format("Cambiar color de relleno de %s", selectedFigure);
+    }
+
 }
