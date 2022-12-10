e1497cce09518d884323bdc4b2eddb705dc1a445
Uriel <urielarias.a12@gmail.com>
2022-12-10 16:07:36 -0300

    prototyping undoable actions

---
+++ src/frontend/CopyAction.java
@@ -0,0 +1,27 @@
+package frontend;
+
+import backend.model.Figure;
+import backend.model.Point;
+
+public class CopyAction {
+    private final Figure selectedFigure;
+    private Figure copiedFigure;
+    private final Point center = new Point(400, 300);
+
+    CopyAction(Figure selectedFigure){
+        this.selectedFigure = selectedFigure;
+    }
+
+    public void press(){
+        this.copiedFigure = selectedFigure.getDuplicate(center);
+        this.copiedFigure.copyFormat(selectedFigure);
+    }
+
+    public void undo(){
+        this.copiedFigure = null;
+    }
+
+    public String toString(){
+        return String.format("Copiar %s", selectedFigure);
+    }
+}
