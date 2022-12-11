df5f0fc894915baa48cb5e45da2af68188230af0
solrodriguez <solrodriguez@itba.edu.ar>
2022-12-11 16:07:49 -0300

    CopyFormatAction

--- src/frontend/actions/CopyFormatAction.java
+++ src/frontend/actions/CopyFormatAction.java
@@ -3,9 +3,10 @@ package frontend.actions;
 import backend.CanvasState;
 import backend.model.Figure;
 import backend.model.Point;
+import frontend.interfaces.Undoable;
 import javafx.scene.paint.Color;
 
-public class CopyFormatAction {
+public class CopyFormatAction implements Undoable {
 
     private Color newLineColor;
     private Color newFillColor;
@@ -13,8 +14,12 @@ public class CopyFormatAction {
     private Figure selectedFigure;
     private CanvasState canvasState;
     private Point eventPoint;
+    private Color oldLineColor;
+    private Color oldFillColor;
+    private double oldLineWidth;
+    private Figure auxFigure;
 
-    public CopyFormatAction(Figure selectedFigure, CanvasState canvasState, Point eventPoint, Color newLineColor, Color newFillColor, double newLineWidth){
+    public CopyFormatAction(Figure selectedFigure, CanvasState canvasState, Point eventPoint, Color newLineColor, Color newFillColor, double newLineWidth) {
         this.canvasState = canvasState;
         this.newLineColor = newLineColor;
         this.newFillColor = newFillColor;
@@ -26,13 +31,21 @@ public class CopyFormatAction {
     public void press(){
         for (Figure figure : canvasState.figures()) {
             if (figure.belongs(eventPoint)) {
+                auxFigure = figure;
+                oldFillColor = figure.getFillColor();
+                oldLineColor = figure.getLineColor();
+                oldLineWidth = figure.getLineWidth();
                 figure.setFormat(newLineColor, newFillColor, newLineWidth);
             }
         }
     }
 
     public void undo() {
-
+        for (Figure figure : canvasState.figures()) {
+            if (figure == auxFigure) {
+                figure.setFormat(oldLineColor, oldFillColor, oldLineWidth);
+            }
+        }
     }
 
     public String toString() {
