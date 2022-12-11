78c4366d5a446f82b153f700d5d1e640eaaabc7f
agussang <msanguinetti@itba.edu.ar>
2022-12-11 13:42:46 -0300

    pasteAction. OJO Dudoso el undo()

--- src/frontend/actions/PasteAction.java
+++ src/frontend/actions/PasteAction.java
@@ -1,13 +1,32 @@
 package frontend.actions;
 
-import frontend.actions.CopyAction;
+import backend.CanvasState;
+import backend.model.Figure;
 
-public class PasteAction {
+public class PasteAction extends CopyAction {
 
     private CopyAction previousCopy;
-    private
+    private Figure copyFigure;
+    private final CanvasState canvasState;
 
-    PasteAction(){
+    public PasteAction(Figure copyFigure, CanvasState canvasState){
+        //this.copiedFigure= copiedFigure;
+        super(copyFigure);
+        this.canvasState = canvasState;
+    }
 
+    @Override
+    public void press() {
+        //Figure aux = copiedFigure.getDuplicate(new Point(400,300));
+        //aux.setFormat(copiedFigure.getLineColor(),copiedFigure.getFillColor(),copiedFigure.getLineWidth());
+        super.press();
+        canvasState.addFigure(getCopiedFigure());
+    }
+
+    @Override
+    public void undo() {
+        DeleteFigureAction delete = new DeleteFigureAction(getCopiedFigure(), canvasState);
+        delete.press();
+        //dudoso
     }
 }
